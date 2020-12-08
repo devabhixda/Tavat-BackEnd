0de7f851c05dca625d42cc3d945535243c14501f
Abhi Jain <devabhixda@gmail.com>
2020-12-08 09:06:11 +0530

    addChatRoom function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
+exports.addChatRoom = functions.https.onCall((data, context) => {
+  const ChatRoom = admin.firestore().collection("chatRoom").doc(data["chatRoomId"]);
+  return ChatRoom.set({
+      chatRoom: data["chatRoom"]
+  });
+});
